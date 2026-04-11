d569e2036eff7d199730577314c12c6235de4e40
Jason Paul Miller <jasonversamiller@gmail.com>
2026-04-11 21:12:00 +0100

    added

--- tpu_inference/layers/vllm/quantization/__init__.py
+++ tpu_inference/layers/vllm/quantization/__init__.py
@@ -22,8 +22,11 @@ from vllm.model_executor.layers.quantization.base_config import \
 
 from tpu_inference.layers.common import quant_methods
 from tpu_inference.layers.vllm.quantization.awq import VllmAWQConfig
-from tpu_inference.layers.vllm.quantization.compressed_tensors.compressed_tensors import \
-    VllmCompressedTensorsConfig
+try:
+    from tpu_inference.layers.vllm.quantization.compressed_tensors.compressed_tensors import \
+        VllmCompressedTensorsConfig
+except Exception:  # pragma: no cover - depends on vLLM import compatibility.
+    VllmCompressedTensorsConfig = None
 from tpu_inference.layers.vllm.quantization.configs import VllmQuantConfig
 from tpu_inference.layers.vllm.quantization.fp8 import VllmFp8Config
 from tpu_inference.layers.vllm.quantization.mxfp4 import VllmMxfp4Config
@@ -45,11 +48,12 @@ def get_tpu_quantization_config(vllm_config: VllmConfig,
     # TODO(kyuyeunk): Add support for "tpu_int8".
     method_to_config: dict[str | None, Type[QuantizationConfig]] = {
         None: VllmUnquantizedConfig,
-        quant_methods.COMPRESSED_TENSORS: VllmCompressedTensorsConfig,
         quant_methods.AWQ: VllmAWQConfig,
         quant_methods.FP8: VllmFp8Config,
         quant_methods.MXFP4: VllmMxfp4Config,
     }
+    if VllmCompressedTensorsConfig is not None:
+        method_to_config[quant_methods.COMPRESSED_TENSORS] = VllmCompressedTensorsConfig
     if model_config.quantization not in method_to_config:
         raise NotImplementedError(
             f"{model_config.quantization} quantization method not supported."
